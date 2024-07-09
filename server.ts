9515f08a7a6e35562eb7478ea6e1e804b32df74d
Asfandyar <asfandm17@gmail.com>
2024-07-10 01:51:09 +0500

    creating server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("server file running!");
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3001;
+
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+};
+
+startServer();
